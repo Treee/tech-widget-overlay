5130324bc7a4302bff7b3ac28f2c2f67ceee1105
Treee <jonathon.lindemann@gmail.com>
2020-05-29 16:55:31 -0600

    changing map name display font color to white with black border

--- src/components/client-overlays/scoreboard-overlay/CurrentMapDisplay.vue
+++ src/components/client-overlays/scoreboard-overlay/CurrentMapDisplay.vue
@@ -30,5 +30,7 @@ export default {
   font-family: "Memb-Font";
   margin-left: 0.5rem;
   margin-right: 0.5rem;
+  text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;
+  color: white;
 }
 </style>
