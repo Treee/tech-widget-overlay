d01ee57b44e34190361d679f4235ae0d37e69802
Treee <twitch.itsatreee@gmail.com>
2020-11-29 17:39:02 -0700

    added disabled buttons when selected. added clear rounds button. added add round button

--- src/components/admin/Admin.vue
+++ src/components/admin/Admin.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="admin-page">
-    <!-- <RoundOverlay/> -->
+    <RoundOverlay />
     <AdminTechUpgradeOverlay
       v-on:techOverlayShow="techOverlayHandler"
       v-on:techOverlayClearAll="techClearAllHandler"
@@ -28,7 +28,9 @@
         Age of Empires II © Microsoft Corporation.
         <b>AoE Tech/Map Overlay for Age of Empires II</b> was created under Microsoft's
         "
-        <a href="https://www.xbox.com/en-us/developers/rules">Game Content Usage Rules</a>"
+        <a
+          href="https://www.xbox.com/en-us/developers/rules"
+        >Game Content Usage Rules</a>"
         using assets from Age of Empires II, and it is not endorsed by or affiliated with Microsoft.
       </div>
     </div>
@@ -36,80 +38,80 @@
 </template>
 
 <script>
-  import AdminTechUpgradeOverlay from "./tech-upgrade-overlay/AdminTechUpgradeOverlay.vue";
-  import NewRoundModal from "./map-pick-ban-overlay/NewRoundModal.vue";
-  import MapListDisplay from "./map-pick-ban-overlay/MapListDisplay.vue";
-  import PlayerCivDisplayControls from "./misc-overlay/PlayerCivDisplayControls.vue";
-  // import RoundOverlay from "./round-overlay/RoundOverlay.vue";
+import AdminTechUpgradeOverlay from "./tech-upgrade-overlay/AdminTechUpgradeOverlay.vue";
+import NewRoundModal from "./map-pick-ban-overlay/NewRoundModal.vue";
+import MapListDisplay from "./map-pick-ban-overlay/MapListDisplay.vue";
+import PlayerCivDisplayControls from "./misc-overlay/PlayerCivDisplayControls.vue";
+import RoundOverlay from "./round-overlay/RoundOverlay.vue";
 
-  import adminOverlayWebSocket from "../../client";
-  import { SocketEnums } from "../../socket-enums";
-  export default {
-    name: "Admin",
-    props: {
-      clientId: String,
+import adminOverlayWebSocket from "../../client";
+import { SocketEnums } from "../../socket-enums";
+export default {
+  name: "Admin",
+  props: {
+    clientId: String
+  },
+  components: {
+    AdminTechUpgradeOverlay,
+    NewRoundModal,
+    MapListDisplay,
+    PlayerCivDisplayControls,
+    RoundOverlay
+  },
+  created: function() {
+    this.adminClient = adminOverlayWebSocket.startClient(this.clientId, () => {
+      // this function would normally handle messages from the sebsocket server.
+      // The admin widget is purely one way outbound so this is empty
+    });
+  },
+  methods: {
+    techOverlayHandler() {
+      const data = this.$store.getters.getTechOverlayData;
+      this.adminClient.sendMessage(SocketEnums.AdminShow, { ...data });
     },
-    components: {
-      AdminTechUpgradeOverlay,
-      NewRoundModal,
-      MapListDisplay,
-      PlayerCivDisplayControls,
-      // RoundOverlay
+    techClearAllHandler() {
+      this.adminClient.sendMessage(SocketEnums.AdminHide, {});
     },
-    created: function () {
-      this.adminClient = adminOverlayWebSocket.startClient(this.clientId, () => {
-        // this function would normally handle messages from the sebsocket server.
-        // The admin widget is purely one way outbound so this is empty
+    mapOverlayShow() {
+      this.adminClient.sendMessage(SocketEnums.AdminShowMaps, {
+        mapData: this.$store.getters.getMapData,
+        players: this.$store.getters.getPlayerNames
       });
     },
-    methods: {
-      techOverlayHandler() {
-        const data = this.$store.getters.getTechOverlayData;
-        this.adminClient.sendMessage(SocketEnums.AdminShow, { ...data });
-      },
-      techClearAllHandler() {
-        this.adminClient.sendMessage(SocketEnums.AdminHide, {});
-      },
-      mapOverlayShow() {
-        this.adminClient.sendMessage(SocketEnums.AdminShowMaps, {
-          mapData: this.$store.getters.getMapData,
-          players: this.$store.getters.getPlayerNames,
-        });
-      },
-      mapOverlayHide() {
-        this.adminClient.sendMessage(SocketEnums.AdminHideMaps, {});
-      },
-      miscOverlayBroadcast() {
-        const data = this.$store.getters.getMiscOverlayData;
-        this.adminClient.sendMessage(SocketEnums.AdminShowCiv, { ...data });
-      },
-      scoreboardMapChange() {
-        const data = {
-          showCurrentMapName: this.$store.getters.getMiscOverlayData
-            .showCurrentMapName,
-          currentMap: this.$store.getters.getMiscOverlayData.currentMap,
-        };
-        this.adminClient.sendMessage(SocketEnums.AdminShowDock, {
-          ...data,
-        });
-      },
+    mapOverlayHide() {
+      this.adminClient.sendMessage(SocketEnums.AdminHideMaps, {});
     },
-  };
+    miscOverlayBroadcast() {
+      const data = this.$store.getters.getMiscOverlayData;
+      this.adminClient.sendMessage(SocketEnums.AdminShowCiv, { ...data });
+    },
+    scoreboardMapChange() {
+      const data = {
+        showCurrentMapName: this.$store.getters.getMiscOverlayData
+          .showCurrentMapName,
+        currentMap: this.$store.getters.getMiscOverlayData.currentMap
+      };
+      this.adminClient.sendMessage(SocketEnums.AdminShowDock, {
+        ...data
+      });
+    }
+  }
+};
 </script>
 
 <style language="scss">
-  .admin-page {
-    user-select: none;
-    background-color: burlywood;
-    height: 100vh;
-    overflow: hidden;
-  }
+.admin-page {
+  user-select: none;
+  background-color: burlywood;
+  height: 100vh;
+  overflow: hidden;
+}
 
-  .my-footer {
-    text-shadow: none;
-    text-align: center;
-    position: absolute;
-    width: 100%;
-    bottom: 0;
-  }
+.my-footer {
+  text-shadow: none;
+  text-align: center;
+  position: absolute;
+  width: 100%;
+  bottom: 0;
+}
 </style>
--- src/components/admin/round-overlay/RoundOverlay.vue
+++ src/components/admin/round-overlay/RoundOverlay.vue
@@ -1,54 +1,83 @@
 <template>
-  <div class="">
-    <md-button class="md-raised vs1" :class="this.isRoundModeSelected(0) ? 'active-round' : ''" :disabled="this.isRoundModeSelected(0)" @click="selectRoundMode(0);">1v1</md-button>
-    <md-button class="md-raised vs2" :class="this.isRoundModeSelected(1) ? 'active-round' : ''" :disabled="this.isRoundModeSelected(1)" @click="selectRoundMode(1);">2v2</md-button>
-    <md-button class="md-raised vs3" :class="this.isRoundModeSelected(2) ? 'active-round' : ''" :disabled="this.isRoundModeSelected(2)" @click="selectRoundMode(2);">3v3</md-button>
-    <md-button class="md-raised vs4" :class="this.isRoundModeSelected(3) ? 'active-round' : ''" :disabled="this.isRoundModeSelected(3)" @click="selectRoundMode(3);">4v4</md-button>
+  <div>
+    <div>
+      <label>Match Mode</label>
+      <br />
+      <md-button
+        class="md-raised vs1"
+        :class="this.isRoundModeSelected(0) ? 'active-round' : ''"
+        :disabled="this.isRoundModeSelected(0)"
+        @click="selectRoundMode(0);"
+      >1v1</md-button>
+      <md-button
+        class="md-raised vs2"
+        :class="this.isRoundModeSelected(1) ? 'active-round' : ''"
+        :disabled="this.isRoundModeSelected(1)"
+        @click="selectRoundMode(1);"
+      >2v2</md-button>
+      <md-button
+        class="md-raised vs3"
+        :class="this.isRoundModeSelected(2) ? 'active-round' : ''"
+        :disabled="this.isRoundModeSelected(2)"
+        @click="selectRoundMode(2);"
+      >3v3</md-button>
+      <md-button
+        class="md-raised vs4"
+        :class="this.isRoundModeSelected(3) ? 'active-round' : ''"
+        :disabled="this.isRoundModeSelected(3)"
+        @click="selectRoundMode(3);"
+      >4v4</md-button>
+    </div>
+    <div class="round-controls">
+      <md-button class="md-raised md-accent" @click="clearRounds();">Clear Rounds</md-button>
+      <md-button class="md-raised" @click="addRound();">Add Round</md-button>
+    </div>
   </div>
 </template>
 
 <script>
-  export default {
-    name: "RoundOverlay",
-    data() {
-      return {
-        roundMode: 0
-      };
+export default {
+  name: "RoundOverlay",
+  data() {
+    return {
+      roundMode: 0
+    };
+  },
+  components: {},
+  methods: {
+    selectRoundMode(roundType) {
+      this.roundMode = roundType;
+      this.$store.dispatch("deleteRound", {
+        mapIdToDelete: this.id
+      });
     },
-    components: {
+    isRoundModeSelected(roundType) {
+      return this.roundMode === roundType;
     },
-    methods: {
-      selectRoundMode(roundType) {
-        this.roundMode = roundType;
-        this.$store.dispatch("deleteRound", {
-          mapIdToDelete: this.id,
-        });
-      },
-      isRoundModeSelected(roundType) {
-        return this.roundMode === roundType;
-      }
-    }
-  };
+    addRound() {},
+    clearRounds() {}
+  }
+};
 </script>
 
 <style language="scss">
-  .md-raised.active-round {
-    background-color: grey !important;
-  }
-  .vs1 {
-    background-color: cornflowerblue !important;
-    border: 1px solid black;
-  }
-  .vs2 {
-    background-color: hotpink !important;
-    border: 1px solid black;
-  }
-  .vs3 {
-    background-color: greenyellow !important;
-    border: 1px solid black;
-  }
-  .vs4 {
-    background-color: gold !important;
-    border: 1px solid black;
-  }
+.md-raised.active-round {
+  background-color: grey !important;
+}
+.vs1 {
+  background-color: cornflowerblue !important;
+  border: 1px solid black;
+}
+.vs2 {
+  background-color: hotpink !important;
+  border: 1px solid black;
+}
+.vs3 {
+  background-color: greenyellow !important;
+  border: 1px solid black;
+}
+.vs4 {
+  background-color: gold !important;
+  border: 1px solid black;
+}
 </style>
