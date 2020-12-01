c19a1e92cc78e6d22e3dfaa9d12f4ad7309a5cb7
Treee <jonathon.lindemann@gmail.com>
2020-11-30 18:33:27 -0700

    round images

--- src/components/admin/round-overlay/RoundDisplayCard.vue
+++ src/components/admin/round-overlay/RoundDisplayCard.vue
@@ -148,7 +148,7 @@ export default {
       teamTwoName: (state) => state.roundOverlay.team2Name,
     }),
     getMapImage() {
-      const map = this.toKabobCase(this.name);
+      const map = this.name === "" ? 'unknown' : this.toKabobCase(this.name);
       const mapFolder = this.$store.getters.isCustomMap(map)
         ? "custom"
         : "default";
@@ -207,4 +207,44 @@ export default {
 </script>
 
 <style language="scss">
+  .map-display {
+    width: 12rem;
+    height: 12rem;
+  }
+
+  .image-container {
+    width: inherit;
+    height: inherit;
+    display: inline-flex;
+    justify-content: center;
+  }
+
+  .map-frame {
+    width: inherit;
+    height: inherit;
+    background-size: cover !important;
+    position: absolute;
+    z-index: 2;
+  }
+
+  .map-image {
+    width: inherit;
+    height: inherit;
+    background-size: cover !important;
+    background-repeat: round !important;
+    position: absolute;
+    transform: scale(0.9) translateY(-3px) translateX(2px);
+    z-index: 1;
+  }
+
+  .map-name {
+    z-index: 2;
+    font-size: larger;
+    color: whitesmoke;
+    text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;
+    display: inline-flex;
+    height: fit-content;
+    position: relative;
+    top: 86%;
+  }
 </style>
